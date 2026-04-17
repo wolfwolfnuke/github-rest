a00df3d16740fe5986d38b5c94ca3b738e233fdd
Sam Lamberton <97002598+wolfwolfnuke@users.noreply.github.com>
2026-04-17 20:10:15 +0000

    fixed?

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
 from fastapi import FastAPI
 import uvicorn
 import discord
 import asyncio
 import threading
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,11 +17,16 @@ def hello():
 @app.get("/double")
 def double(x: int):
     return {"result": x * 2}
-
-#t1 = threading.Thread(target=client.run())
-t2 = threading.Thread(target=uvicorn.run(app))
+token = os.environ['DISCORDTOKEN']
+t1 = threading.Thread(target=lambda: client.run(token))
+t2 = threading.Thread(target=lambda: uvicorn.run(app))
 #uvicorn.run(app)
-print("starting uvicornthread")
+print("starting discord thread")
+t1.start()
+print("started discord thread")
+print("starting uvicorn thread")
 t2.start()
 print("started uvicorn thread")
+t1.join()
 t2.join()
+
